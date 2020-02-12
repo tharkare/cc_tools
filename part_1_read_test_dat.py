458ab964ae1d40aea8178cb860335a228de54852
Tanvi H <tharkare@andrew.cmu.edu>
2020-02-12 16:22:12 -0500

    Update part_1_read_test_dat.py

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -3,6 +3,8 @@ import cc_dat_utils
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
-
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+make_cc_level_pack_from_dat(pfgd_test)
+print data
