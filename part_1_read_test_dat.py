a6e5c88d10b00b72fa1d695b0849a8951b9a1a8f
Tanvi H <tharkare@andrew.cmu.edu>
2020-02-12 18:36:26 -0500

    final changes to py files

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,4 +1,4 @@
-import cc_dat_utils
+from cc_dat_utils import make_cc_level_pack_from_dat
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
@@ -6,5 +6,8 @@ input_dat_file = "data/pfgd_test.dat"
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
 
-make_cc_level_pack_from_dat(pfgd_test)
-print data
+pfgd_test = make_cc_level_pack_from_dat(input_dat_file)
+print(pfgd_test)
+newfile = open('data/pfgd_test.txt', 'w') #created a new blank file
+newfile.write(str(pfgd_test)) #placed data into the file
+newfile.close() #closed the file
