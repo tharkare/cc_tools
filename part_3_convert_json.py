f934683db96cdeffab0396f24b614e9597a79597
Tanvi H <tharkare@andrew.cmu.edu>
2020-02-22 10:40:58 -0500

    trying to make the json file to print

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,6 +1,17 @@
 import cc_dat_utils
+import cc_classes
+import json
 
 #Part 3
 #Load your custom JSON file
+json_file_name = "tharkare_cc1.json"
+with open(json_file_name, 'r') as reader:
+    game_json_data = json.load(reader)
+
 #Convert JSON data to CCLevelPack
+new_level_pack = cc_classes.CCLevelPack()
+new_level = cc_classes.CCLevel()
+new_level_pack.add_level(new_level)
+print(new_level_pack)
+
 #Save converted data to DAT file
