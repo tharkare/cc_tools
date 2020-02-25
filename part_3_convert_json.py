8e308a54aa89be80f732bf8be22d945bda1eb4a9
Tanvi H <tharkare@andrew.cmu.edu>
2020-02-24 22:01:45 -0500

    working without optional fields

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -13,23 +13,21 @@ level_number_26 = game_json_data[0]["level_number"]
 time_26 = game_json_data[0]["time"]
 num_chips_26 = game_json_data[0]["num_chips"]
 upper_layer_26 = game_json_data[0]["upper_layer"]
+lower_layer_26 = game_json_data[0]["lower_layer"]
 optional_fields_26 = game_json_data[0]["optional_fields"]
 
 new_level_pack = cc_classes.CCLevelPack()
 new_level_26 = cc_classes.CCLevel()
+new_clone_26 = cc_classes.CCCloningMachineControl(5, 5, 15, 15)
 
 new_level_26.level_number = level_number_26
 new_level_26.time = time_26
 new_level_26.num_chips = num_chips_26
 new_level_26.upper_layer = upper_layer_26
-new_level_26.optional_fields = optional_fields_26
+#new_level_26.optional_fields = optional_fields_26
 
 new_level_pack.add_level(new_level_26)
-print(new_level_pack)
 
 #Save converted data to DAT file
-#cc_level_pack = cc_dat_utils.make_cc_level_pack_from_dat("data/pfgd_test.dat")
-#cc_dat_utils.write_cc_level_pack_to_dat(cc_level_pack, "data/TEST.dat")
-
-
-#final_level = cc_dat_utils.write_level_to_dat(new_level_26, "data/final_level.dat")
+file_writer = open("data/final_level.dat", "wb")
+final_level = cc_dat_utils.write_level_to_dat(new_level_26, file_writer)
